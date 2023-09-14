e2d2e8ed357ed606183876154c7fe9ac062dd3e7
Wider <wider93@naver.com>
2023-09-15 03:11:44 +0900

    added kmp

---
+++ Lib/string/kmp.h
@@ -0,0 +1,31 @@
+#include "vector"
+#include "ranges"
+using namespace std;
+
+template<random_access_iterator It>
+struct kmp{
+    int n;
+    using T = remove_reference_t<decltype (* ((It)nullptr))>;
+    vector<T> needle;
+    vector<int> fail;
+    kmp(It b, It e):n(e - b), needle(b, e), fail(n){
+        for(int j = 0, i = 1; i < n; ++i){
+            while(j > 0 && b[i] != b[j]) j = fail[j-1];
+            if (b[i] == b[j]) ++j, fail[i] = j;
+            else fail[i] = 0;
+        }
+    }
+    template<random_access_iterator It2>
+    requires (same_as<T, remove_reference_t<decltype (* ((It2)nullptr))>>)
+    vector<int> search(It2 b, It2 e){
+        int m = distance(b, e) - n + 1;
+        vector<int> ans;
+		for(int j = 0; b < e; ++b){
+			while (j > 0 && !(j < n && needle[j] == *b))
+				j = fail[j-1];
+			if(j < n && needle[j] == *b){
+				if (j + 1 == n) ans.push_back(m - distance(b, e));
+			}++j;
+		} return ans;
+    }
+};
